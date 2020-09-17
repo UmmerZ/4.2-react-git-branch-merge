7d182ff537acb7f47f19e81bfecc506d03a1c34f
ummerZ <ummer.zaman88@gmail.com>
2020-09-17 11:11:52 -0600

    added hello student component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import HelloWorld from './components/HelloWorld';
+import HelloStudent from './components/HelloStudent';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>React Git Branch Merge Practice</h1>
-    <HelloWorld/>
+    <h1>My Git Test</h1>
+    <HelloStudent name="Eboka"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
